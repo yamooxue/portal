bd069657d94ab4ca76edcaa755dd958eb83ce98b
fuxiaoxue <fuxiaoxue@jiangroom.com>
2020-03-18 17:01:29 +0800

    build

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,20 @@
 module.exports = {
+  // 基本路径
+  baseUrl: '/',
+  // 输出文件目录
+  outputDir: 'dist',
+  // eslint-loader 是否在保存的时候检查
   devServer: {
     disableHostCheck: true,
+    // open: process.platform === 'darwin',
+    // host: '0.0.0.0',
+    port: 8080,
+    https: false,
+    hotOnly: false,
+    proxy: null, // 设置代理
+    before: app => {},
   },
-
   transpileDependencies: ['vuetify'],
-
   pluginOptions: {
     i18n: {
       locale: 'en',
